6d2e7a9f11568fdaf5fc4a7bf114e64ed5f5a954
Nitesh Kant <nitesh1706@gmail.com>
2013-04-03 15:58:42 -0700

    Extract eureka integration as a separate module

--- karyon-eureka/src/main/java/com/netflix/karyon/server/eureka/EurekaHandler.java
+++ karyon-eureka/src/main/java/com/netflix/karyon/server/eureka/EurekaHandler.java
@@ -56,10 +56,7 @@ public class EurekaHandler implements ServiceRegistryClient {
 
     protected static final Logger logger = LoggerFactory.getLogger(EurekaHandler.class);
 
-    @Inject(optional = true)
     private EurekaHealthCheckCallback eurekaHealthCheckCallback;
-
-    @Inject(optional = true)
     private HealthCheckInvocationStrategy healthCheckInvocationStrategy;
 
     private AtomicBoolean registered = new AtomicBoolean();
@@ -81,6 +78,13 @@ public class EurekaHandler implements ServiceRegistryClient {
             documentation = "Datacenter type used for initializing appropriate eureka instance configuration."
     )
     private String datacenterType;
+
+    @Inject
+    public EurekaHandler(EurekaHealthCheckCallback eurekaHealthCheckCallback,
+                         HealthCheckInvocationStrategy healthCheckInvocationStrategy) {
+        this.eurekaHealthCheckCallback = eurekaHealthCheckCallback;
+        this.healthCheckInvocationStrategy = healthCheckInvocationStrategy;
+    }
 
     @PostConstruct
     public void postConfig() {
