48a8a5d84e6af243b4744c99b16e7a1fead0f11f
Tomasz Bak <tbak@netflix.com>
2014-08-23 23:45:17 -0700

    WebSockets governator module.

---
+++ karyon-governator/src/main/java/com/netflix/karyon/transport/http/websockets/WebSocketsRxServerProvider.java
@@ -0,0 +1,83 @@
+package com.netflix.karyon.transport.http.websockets;
+
+import javax.annotation.PreDestroy;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Provider;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+import com.netflix.karyon.transport.AbstractServerModule.ServerConfig;
+import com.netflix.karyon.transport.http.websockets.KaryonWebSocketsModule.WebSocketsServerConfig;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.reactivex.netty.RxNetty;
+import io.reactivex.netty.channel.ConnectionHandler;
+import io.reactivex.netty.metrics.MetricEventsListenerFactory;
+import io.reactivex.netty.pipeline.PipelineConfigurator;
+import io.reactivex.netty.protocol.http.websocket.WebSocketServerBuilder;
+import io.reactivex.netty.server.RxServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.netflix.karyon.utils.TypeUtils.*;
+
+/**
+ * @author Tomasz Bak
+ */
+public class WebSocketsRxServerProvider<I extends WebSocketFrame, O extends WebSocketFrame, S extends RxServer<I, O>> implements Provider<S> {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketsRxServerProvider.class);
+
+    private final Named nameAnnotation;
+
+    protected final Key<ConnectionHandler<I, O>> connectionHandlerKey;
+    private final Key<PipelineConfigurator> pipelineConfiguratorKey;
+    private final Key<MetricEventsListenerFactory> metricEventsListenerFactoryKey;
+    private final Key<ServerConfig> serverConfigKey;
+
+    private RxServer<I, O> server;
+
+    public WebSocketsRxServerProvider(String name, Class<I> iType, Class<O> oType) {
+        nameAnnotation = Names.named(name);
+
+        connectionHandlerKey = keyFor(ConnectionHandler.class, iType, oType, nameAnnotation);
+        pipelineConfiguratorKey = Key.get(PipelineConfigurator.class, nameAnnotation);
+        metricEventsListenerFactoryKey = Key.get(MetricEventsListenerFactory.class, nameAnnotation);
+        serverConfigKey = Key.get(ServerConfig.class, nameAnnotation);
+    }
+
+    @Override
+    public S get() {
+        return (S) server;
+    }
+
+    @PreDestroy
+    public void shutdown() throws InterruptedException {
+        if (server != null) {
+            logger.info("Starting WebSockets server {} on port {}...", nameAnnotation.value(), server.getServerPort());
+            server.shutdown();
+        }
+    }
+
+    @Inject
+    public void setInjector(Injector injector) {
+        WebSocketsServerConfig config = (WebSocketsServerConfig) injector.getInstance(serverConfigKey);
+
+        ConnectionHandler<I, O> connectionHandler = injector.getInstance(connectionHandlerKey);
+
+        WebSocketServerBuilder<I, O> builder = RxNetty.newWebSocketServerBuilder(config.getPort(), connectionHandler)
+                .withMessageAggregator(config.isMessageAggregator());
+
+        if (injector.getExistingBinding(pipelineConfiguratorKey) != null) {
+            builder.appendPipelineConfigurator(injector.getInstance(pipelineConfiguratorKey));
+        }
+
+        if (injector.getExistingBinding(metricEventsListenerFactoryKey) != null) {
+            builder.withMetricEventsListenerFactory(injector.getInstance(metricEventsListenerFactoryKey));
+        }
+
+        server = builder.build().start();
+        logger.info("Starting WebSockets server {} on port {}...", nameAnnotation.value(), server.getServerPort());
+    }
+}
