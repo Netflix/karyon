bbb96d47e59ef72814dd008dbe06dd0a25406220
elandau <elandau@yahoo.com>
2015-08-12 16:06:18 -0700

    Fix unit test

--- karyon3-core/src/test/java/com/netflix/karyon/admin/HealthCheckBindingTest.java
+++ karyon3-core/src/test/java/com/netflix/karyon/admin/HealthCheckBindingTest.java
@@ -6,10 +6,13 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Key;
 import com.google.inject.Provides;
-import com.netflix.governator.DefaultModule;
+import com.google.inject.name.Names;
 import com.netflix.governator.Governator;
 import com.netflix.governator.LifecycleInjector;
+import com.netflix.karyon.healthcheck.CompositeHealthCheck;
 import com.netflix.karyon.healthcheck.HealthCheck;
 import com.netflix.karyon.healthcheck.HealthCheckRegistry;
 import com.netflix.karyon.healthcheck.HealthChecks;
@@ -19,7 +22,7 @@ public class HealthCheckBindingTest {
     @Test
     public void test() {
         LifecycleInjector injector = Governator.createInjector(
-            new DefaultModule() {
+            new AbstractModule() {
                 @Provides
                 @Named("hc1")
                 public HealthCheck getHealthCheck1() {
@@ -29,17 +32,27 @@ public class HealthCheckBindingTest {
                 @Provides
                 @Named("hc2")
                 public HealthCheck getHealthCheck2() {
-                    return HealthChecks.alwaysHealthy(); 
+                    return HealthChecks.alwaysUnhealthy(); 
                 }
+
+                @Override
+                protected void configure() {
+                    bind(HealthCheck.class).to(CompositeHealthCheck.class);
+                }
+                
             });
         
         HealthCheckRegistry registry = injector.getInstance(HealthCheckRegistry.class);
         HealthCheckResource res = injector.getInstance(HealthCheckResource.class);
         
-        HealthStatus status = res.get("hc1");
-        Assert.assertEquals(true, status.isHealthy());
+        HealthStatus status1 = injector.getInstance(Key.get(HealthCheck.class, Names.named("hc1"))).check().join();
+        Assert.assertEquals(true, status1.isHealthy());
         
-        status = res.get("hc2");
+        HealthStatus status2 = injector.getInstance(Key.get(HealthCheck.class, Names.named("hc2"))).check().join();
+        Assert.assertEquals(false, status2.isHealthy());
+        
+        HealthStatus status = injector.getInstance(Key.get(HealthCheck.class)).check().join();
         Assert.assertEquals(false, status.isHealthy());
+        Assert.assertEquals(2, status.getAttributes().size());
     }
 }
