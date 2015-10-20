23d71cf093ac29be4c1ef0b6954735679392d17d
elandau <elandau@yahoo.com>
2015-10-20 12:02:17 -0700

    Introduce 'karyon_admin' configuration file with overrides for 'local' profile.

--- karyon3-admin-simple/src/main/java/com/netflix/karyon/admin/rest/AdminServerConfig.java
+++ karyon3-admin-simple/src/main/java/com/netflix/karyon/admin/rest/AdminServerConfig.java
@@ -18,7 +18,7 @@ public interface AdminServerConfig extends HttpServerConfig {
     /**
      * Admin specific settings
      */
-    @DefaultValue("http://${@localIpv4}:8078/index.html#/${@localIpv4}:8077/info")
+    @DefaultValue("http://${@publicHostname}:8078/index.html#/${@publicHostname}:8077/appinfo")
     String remoteServer();
     
     @DefaultValue("${@hostname}")
