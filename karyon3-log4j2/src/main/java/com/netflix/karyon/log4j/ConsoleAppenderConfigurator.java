f13dda4259b947da99dd627c5942068bdf03b2f4
elandau <elandau@yahoo.com>
2015-10-30 19:52:32 -0700

    Much more configurable Console appender

--- karyon3-log4j2/src/main/java/com/netflix/karyon/log4j/ConsoleAppenderConfigurator.java
+++ karyon3-log4j2/src/main/java/com/netflix/karyon/log4j/ConsoleAppenderConfigurator.java
@@ -1,7 +1,9 @@
 package com.netflix.karyon.log4j;
 
 import java.io.Serializable;
+import java.nio.charset.Charset;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -13,33 +15,50 @@ import org.apache.logging.log4j.core.appender.ConsoleAppender;
 import org.apache.logging.log4j.core.config.AbstractConfiguration;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 
-import com.google.common.base.Charsets;
-import com.netflix.archaius.Config;
+import com.netflix.archaius.ConfigProxyFactory;
+import com.netflix.archaius.annotations.DefaultValue;
 
 @Singleton
 public class ConsoleAppenderConfigurator implements Log4jConfigurator {
-    private static final String PROP_LOG4J_PATTERN = "log4j.appender.Console.pattern";
-    
-    private static final String DEFAULT_PATTERN = "%d{HH:mm:ss,SSS} [%t] %-5p %c %x %m %n";
+    public static interface AppenderConfig {
+        @DefaultValue("%d{HH:mm:ss,SSS} [%t] %-5p %c %x %m %n")
+        String getPattern();
+        
+        @DefaultValue("UTF-8")
+        String getCharset();
+        
+        @DefaultValue("true")
+        Boolean getAlwaysWriteExceptions();
 
-    private final Config _config;
+        @DefaultValue("false")
+        Boolean getNoConsoleNoAnsi();
+
+        @Nullable
+        String getHeader();
+        
+        @Nullable
+        String getFooter();
+    }
+    
+    private final AppenderConfig _config;
     
     @Inject
-    public ConsoleAppenderConfigurator(Config config) {
-        this._config = config;
+    public ConsoleAppenderConfigurator(ConfigProxyFactory factory) {
+        this._config = factory.newProxy(AppenderConfig.class, "log4j.appender.Console");
     }
     
     @Override
     public void doConfigure(AbstractConfiguration config) {
 
-        Layout<? extends Serializable> layout = PatternLayout.createLayout(_config.getString(PROP_LOG4J_PATTERN, DEFAULT_PATTERN), // pattern
+        Layout<? extends Serializable> layout = PatternLayout.createLayout(
+                _config.getPattern(), // pattern
                 config, // config
                 null,   // replace
-                Charsets.UTF_8, // charset
-                true,   // alwaysWriteExceptions
-                false,  // noConsoleNoAnsi
-                null,   // header
-                null);  // footer
+                Charset.forName(_config.getCharset()), // charset
+                _config.getAlwaysWriteExceptions(),   // alwaysWriteExceptions
+                _config.getNoConsoleNoAnsi(),  // noConsoleNoAnsi
+                _config.getHeader(),   // header
+                _config.getFooter());  // footer
 
         // Add the Console appender
         Appender appender;
